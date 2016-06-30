d101a8d1aaa1c09dbfe887424af98513fb62e2b6
sbfnk <sbfnk@users.noreply.github.com>
2016-06-30 08:03:15 +0100

    particles, sample

--- libbi.lofa.sh
+++ libbi.lofa.sh
@@ -21,4 +21,5 @@ echo Seed: $seed
 source ~/.bashrc
 source ~/perl5/perlbrew/etc/bashrc
 
-Rscript ~/code/ebola_lofa/R/sample_posterior.r -n 10000 -p 1000 -r $r0_trajectory -t 4 -e $seed -i 10 -q -l -f -k -o ebola_lofa_poisson
+##Rscript ~/code/ebola_lofa/R/sample_posterior.r -n 10000 -p 1000 -r $r0_trajectory -t 4 -e $seed -i 10 -q -l -f -k -o ebola_lofa_poisson
+Rscript ~/code/ebola_lofa/R/sample_posterior.r -n 100000 -p 1000 -r $r0_trajectory -t 4 -e $seed -i 10 -q -l -f -k -o ebola_lofa_poisson -c 16384
