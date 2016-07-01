985d021c207179f6e3ae18ade36f67a904d9e326
sbfnk <sbfnk@users.noreply.github.com>
2016-07-01 07:44:22 +0100

    parallel

--- libbi.lofa.sh
+++ libbi.lofa.sh
@@ -3,9 +3,10 @@
 #$ -V -cwd
 #$ -M sebastian.funk@lshtm.ac.uk
 #$ -l mem_free=4000M,h_vmem=8000M
-#$ -pe smp 4
+#$ -pe smp 8
 #$ -q parallel.q
 #$ -R y
+#$ -t 1-10
 
 r0_trajectory=$1
 if [ -z "$r0_trajectory" ]
@@ -22,4 +23,4 @@ source ~/.bashrc
 source ~/perl5/perlbrew/etc/bashrc
 
 ##Rscript ~/code/ebola_lofa/R/sample_posterior.r -n 10000 -p 1000 -r $r0_trajectory -t 4 -e $seed -i 10 -q -l -f -k -o ebola_lofa_poisson
-Rscript ~/code/ebola_lofa/R/sample_posterior.r -n 100000 -p 1000 -r $r0_trajectory -t 4 -e $seed -i 10 -q -l -f -k -o ebola_lofa_poisson -c 16384
+Rscript ~/code/ebola_lofa/R/sample_posterior.r -n 10000 -p 1000 -r $r0_trajectory -t 8 -e $seed -i 10 -q -l -f -o ebola_lofa_$r0_trajectory\_poisson_${SGE_TASK_ID} -c 16384 -b ${SGE_TASK_ID} 
