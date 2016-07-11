859173c67ca4d071bbee65b7f5daa9ec6377cbae
sbfnk <sbfnk@users.noreply.github.com>
2016-07-11 18:32:38 +0100

    get input from traces file

--- R/scenarios.R
+++ R/scenarios.R
@@ -1,54 +1,19 @@
 library('RBi')
 library('data.table')
-library('dplyr')
 
-code_dir <- path.expand("~/code/ebola_lofa/")
+code_dir <- "~/code/ebola_lofa"
+output_folder <- path.expand("~/Data/Ebola/Lofa")
 
 ## create working folder
-output_folder <- path.expand("~/Data/Ebola/Lofa")
-master_working_folder <- paste(output_folder, "libbi", sep = "/")
-suppressWarnings(dir.create(master_working_folder))
-working_folder <- paste(master_working_folder, "bounded", sep = "/")
+working_folder <- paste(output_folder, "Scenarios", sep = "/")
 unlink(working_folder, recursive = TRUE)
 dir.create(working_folder)
 
 ## get posterior file
-posterior <- readRDS("lofa_combined_traces.rds")
+combined <-
+  readRDS(paste(output_folder, "lofa_combined_traces.rds", sep = "/"))
 
-## get original input file
-min_date <- as.Date("2014-06-01")
-max_date <- as.Date("2014-10-20")
-rate_multiplier <- 7
-
-## read incidence data
-inc_filename <- paste0("incidence_weekly.rds")
-incidence <- readRDS(paste(code_dir, "data", inc_filename, sep = "/"))
-
-## read admission dates
-adm_filename <- paste0("admission_delays_weekly.rds")
-admission_delays <- readRDS(paste(code_dir, "data", adm_filename, sep = "/"))
-admission_delays <- admission_delays %>%
-  mutate(admission.delay = admission.delay)
-
-admission_dates <-
-  data.frame(date = seq.Date(min_date, max_date, by = "week"))
-admissions_data <- admission_dates %>%
-  left_join(incidence[["admissions"]], by = "date") %>%
-  left_join(incidence[["deaths"]], by = "date") %>%
-  filter(between(date, min_date, max_date)) %>%
-  mutate(time = as.integer(((date - min(date)) / rate_multiplier + 1)) - 1) %>%
-  mutate(admissions = ifelse(is.na(admissions), 0, admissions)) %>%
-  mutate(deaths = ifelse(is.na(deaths), 0, deaths))
-
-delay_dates <-
-  data_frame(date = seq.Date(min(admissions_data$date),
-                             max(admissions_data$date), by = "week"))
-delay_dates$value <-
-  approx(x = admission_delays$date,
-         y = admission_delays$admission.delay,
-         xout = delay_dates$date)$y
-delay_dates <- delay_dates %>% 
-  mutate(time = as.integer(((date - min(date)) / rate_multiplier)))
+posterior <- copy(combined$traces)
 
 posterior$R0[, time := time - 1]
 posterior$R0 <- posterior$R0[time >= 0]
@@ -57,24 +22,24 @@ posterior$H <- posterior$H[time >= 0]
 posterior$n_admission[, time := time - 1]
 posterior$n_admission <- posterior$n_admission[time >= 0]
 
-
 ## read bed capacity
-bed_filename <- paste0("lofa_etc_weekly.rds")
+bed_filename <- "lofa_etc_weekly.rds"
 bed_availability <- readRDS(paste(code_dir, "data", bed_filename, sep = "/"))
 
-bed_availability <- bed_availability[date >= min_date & date <= max_date]
+bed_availability <- bed_availability[date >= min(combined$data$time) &
+                                     date <= max(combined$data$time)]
 bed_availability[, time := seq_len(nrow(bed_availability)) - 1]
 
 ## combine original input file with R0 trajectories from the posterior
-input <- list(R0 = posterior$R0,
-              H = posterior$H,
-              K = bed_availability[, list(time, value = available.ebola)],
-              admission_delay = delay_dates %>% select(time, value),
-              late_increase = 1,
-              n_admission = posterior$n_admission)
+input <- c(combined$input,
+           list(R0 = posterior$R0,
+                H = posterior$H,
+                K = bed_availability[, list(time, value = available.ebola)],
+                late_increase = 1,
+                n_admission = posterior$n_admission))
 
 ## get model
-ebola_model <- bi_model(paste0(code_dir, "ebola_lofa_sim.bi"))
+ebola_model <- bi_model(paste(code_dir, "ebola_lofa_sim.bi", sep = "/"))
 
 ## remove R0 assign line, this is now from the input file
 r0_assign_line <- grep("^[[:space:]]*R0[[:space:]]*<-", ebola_model$get_lines())
@@ -181,6 +146,7 @@ for (i in seq_along(sim_scenarios)) {
 all_scenarios <- rbindlist(state_scenarios)
 
 saveRDS(all_scenarios, "lofa_scenarios.rds")
+## all_scenarios <- readRDS
 
 admissions_no_change <- all_scenarios[scenario %in% c(6,7) & state == "Admissions",
                                       list(mean = mean(value),
