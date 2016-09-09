fd22366a1686a66bd22c96a6f07520ff86d5607c
sbfnk <sbfnk@users.noreply.github.com>
2016-09-09 13:59:03 +0100

    merge

--- script/libbi.lofa.sh
+++ script/libbi.lofa.sh
@@ -6,7 +6,7 @@
 #$ -pe smp 8
 #$ -q parallel.q
 #$ -R y
-#$ -t 51-100
+#$ -t 1-100
 
 r0_trajectory=$1
 if [ -z "$r0_trajectory" ]
@@ -24,4 +24,4 @@ source ~/perl5/perlbrew/etc/bashrc
 
 ##Rscript ~/code/ebola_lofa/R/sample_posterior.r -n 10000 -p 1000 -r $r0_trajectory -t 4 -e $seed -i 10 -q -l -f -k -o ebola_lofa_poisson
 ##Rscript ~/code/ebola_lofa/R/sample_posterior.r -n 10000 -p 1000 -r $r0_trajectory -t 8 -e $seed -i 10 -q -l -f -g 2 -o ebola_lofa_$r0_trajectory\_poisson_late_${SGE_TASK_ID} -c 16384 -b ${SGE_TASK_ID} 
-Rscript ~/code/ebola_lofa/R/sample_posterior.r -n 10000 -p 1000 -r $r0_trajectory -t 8 -e $seed -i 10 -q -l -f -o ebola_lofa_$r0_trajectory\_poisson_${SGE_TASK_ID} -c 16384 -b ${SGE_TASK_ID} 
+Rscript ~/code/ebola_lofa/R/sample_posterior.r -n 10000 -p 1000 -r $r0_trajectory -t 8 -e $seed -i 10 -q -l -f -o ebola_lofa_${SGE_TASK_ID} -c 16384 -b ${SGE_TASK_ID} 
