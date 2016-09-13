725f00a1db8201e287bf328859475211a674b2db
sbfnk <sbfnk@users.noreply.github.com>
2016-09-13 10:30:03 +0100

    updated analysis

--- R/analyse_parallel_runs.r
+++ R/analyse_parallel_runs.r
@@ -7,7 +7,7 @@ library('cowplot')
 
 output_dir <- path.expand("~/Data/Ebola/Lofa")
 
-n_traj <- 94
+n_traj <- 100
 
 res <- list()
 
@@ -17,7 +17,7 @@ data <- NULL
 
 for (i in seq_len(n_traj))
 {
-    filename <- paste0(output_dir, "/ebola_lofa_independent_", i)
+    filename <- paste0(output_dir, "/ebola_lofa_", i)
     res[[i]] <- readRDS(paste0(filename, ".rds"))
     model <- bi_model(paste0(filename, ".bi"))
     if (is.null(obs)) obs <- readRDS(paste0(filename, "_obs.rds"))
@@ -63,7 +63,7 @@ combined$Admissions[, sd := sqrt(value)]
 combined$Admissions[, value := rtruncnorm(n = .N, a = 0, mean = mean, sd = sd)]
 combined$Admissions[is.na(value), value := 0]
 
-p <- plot_libbi(l, model, data = data, date.origin = as.Date("2014-06-02") - 7, date.unit = "week", data.colour = "black", densities = "histogram", plot = FALSE)
+p <- plot_libbi(combined, model, data = data, date.origin = as.Date("2014-06-02") - 7, date.unit = "week", data.colour = "black", densities = "histogram", plot = FALSE)
 
 combined$p_Inf[, list(mean = mean(value),
                min.50 = quantile(value, 0.25),
@@ -97,9 +97,9 @@ combined$p_vol_R0[, list(mean = mean(value),
 
 beta_params <- c("p_R0", "p_gamma", "p_alpha", "p_cfr", "H")
 
-beta_list <- l[beta_params]
+beta_list <- combined[beta_params]
 beta_list <- lapply(names(beta_list), function(x) {if (!("value" %in% colnames(beta_list[[x]]))) {setnames(beta_list[[x]], "value", x)}})
-l <- lapply(names(l), function(x) {if (!("value" %in% colnames(l[[x]]))) {setnames(l[[x]], x, "value")}})
+combined <- lapply(names(combined), function(x) {if (!("value" %in% colnames(l[[x]]))) {setnames(l[[x]], x, "value")}})
 
 nbl <- copy(beta_list[[1]])
 for (i in seq(2, length(beta_list)))
@@ -110,11 +110,11 @@ for (i in seq(2, length(beta_list)))
 nbl[, beta := p_R0 * p_gamma * p_alpha / (p_alpha + p_cfr * (1 - H) * p_gamma)]
 nbl[, cR0 := beta * (1 / p_gamma + p_cfr/p_alpha)]
 
-l$R0$R0 <- nbl$cR0
-l$p_R0$value <- nbl$cR0
+combined$R0$R0 <- nbl$cR0
+combined$p_R0$value <- nbl$cR0
 
-for (i in names(l)){
-  setnames(l[[i]], i, "value")
+for (i in names(combined)){
+  setnames(combined[[i]], i, "value")
 }
 
 elH <- combined$H[, list(early = value[1], late = value[length(value)]), by = np]
